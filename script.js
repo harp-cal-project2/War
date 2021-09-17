58f7a3bda627cb4496c998855e4a4af3d70d69a1
CalAkler <calvinakler@gmail.com>
2021-09-17 16:53:14 -0400

    add pseudocode

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
-// setup namespace object
-// get card deck (fetch api)
+//MVP:
+// Set up init that calls the get card function 
+
+//  write function to Get card data from deck of cards api
+    // draw 2 cards from draw cards endpoint when the user clicks on the their half deck, which is the event we are listening to. 
+   // display the cards across from one another. 
+ 
+// define a method to compare card values returned from the api response and assign values to the face cards. 
+
+// highest card wins. Ties are just declared. 
+// display result message on page AND reset the game. 
+
+
+
+// Stretch Goals:
+// make a full war game 
+    //Created the winners pile and move winner’s cards to that pile. 
+    // Terminate game when card run out.
+    // Resolve ties. 
+  // Add animations to the cards slide.
+
 
 
 const app = {};
